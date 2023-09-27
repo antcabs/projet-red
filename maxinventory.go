b2cfb6f6d693116705924dcc5b256e7f554f6232
Cabanes Antoine <cabanesantoine@MacBook-Pro-de-Cabanes.local>
2023-09-27 16:10:15 +0200

    ll

---
+++ maxinventory.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+const maxInventoryCapacity = 10
+
+type Player struct {
+    inventory []string
+}
+
+func addItemToInventory(player *Player, item string) string {
+    if len(player.inventory) < maxInventoryCapacity {
+        player.inventory = append(player.inventory, item)
+        return fmt.Sprintf("L'item '%s' a été ajouté à ton sac.", item)
+    }
+    return "Ton sac est déjà plein ! Impossible d'ajouter plus d'items."
+}
+
+func max() {
+    player := Player{}
+
+    // Ajoute 10 items à l'inventaire
+    for i := 1; i <= maxInventoryCapacity; i++ {
+        message := addItemToInventory(&player, fmt.Sprintf("Item %d", i))
+        fmt.Println(message)
+    }
+
+    // pour essayer d'en ajouter un autre
+    message := addItemToInventory(&player, "Item supplémentaire")
+    fmt.Println(message)
+}
